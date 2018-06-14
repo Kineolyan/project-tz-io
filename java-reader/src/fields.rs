e65de411b0b0b7d65aacb95f999f9d78a1144cf5
Kineolyan <kineolyan@gmail.com>
2018-06-14 22:43:13 +0200

    Support field reading.

--- java-reader/src/fields.rs
+++ java-reader/src/fields.rs
@@ -1,12 +1,58 @@
+use flags::to_method_access;
+use pool::{PoolList, resolve_utf8_value};
 use printer::print_bytes;
 use reader::{Reader, ReadResult, to_u16};
+use attributes::read as read_attributes;
 
-pub fn read(reader: &mut Reader) -> ReadResult {
+fn read_access(reader: &mut Reader, indent: u8) -> ReadResult {
+	read_u16!(flag_value, reader, indent);
+	let flags = to_method_access(flag_value);
+	print!("Flags:");
+	for flag in &flags {
+		print!(" {}", flag);
+	}
+	println!("");
+
+	Ok(())
+}
+
+fn read_field_name(reader: &mut Reader, pool: &PoolList, indent: u8) -> ReadResult {
+	read_u16!(name_idx, reader, indent);
+	let method_name = resolve_utf8_value(pool, name_idx as usize)
+		.expect(&format!(
+			"Field name not in the constant pool at {}",
+			name_idx));
+
+	println!("Method '{}'", method_name);
+
+	Ok(())
+}
+
+fn read_descriptor(reader: &mut Reader, pool: &PoolList, indent: u8) -> ReadResult {
+	read_u16!(descriptor_idx, reader, indent);
+	let method_name = resolve_utf8_value(pool, descriptor_idx as usize)
+		.expect(&format!(
+			"Method descriptor not in the constant pool at {}",
+			descriptor_idx));
+
+	println!("Descriptor '{}'", method_name);
+
+	Ok(())
+}
+
+fn read_field(reader: &mut Reader, pool: &PoolList, indent: u8) -> ReadResult {
+	read_access(reader, indent)?;
+	read_field_name(reader, pool, indent)?;
+	read_descriptor(reader, pool, indent + 1)?;
+	read_attributes(reader, pool, indent + 1)
+}
+
+pub fn read(reader: &mut Reader, pool: &PoolList) -> ReadResult {
 	read_u16!(count, reader, 0);
 	println!("Field count = {}", count);
 
-	if count > 0 {
-		panic!("Class defining {} field(s). Not supporting this", count);
+	for _i in 0..count {
+		read_field(reader, pool, 1)?;
 	}
 
 	Ok(())

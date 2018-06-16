cf7a4a2b1632e1d8896df357ea85230ae289ec36
Kineolyan <kineolyan@gmail.com>
2018-06-16 19:43:04 +0200

    Decode more operations.

--- java-reader/src/operations.rs
+++ java-reader/src/operations.rs
@@ -15,15 +15,22 @@ pub fn read(
     read_u8!(operation_code, reader, indent);
     match operation_code {
       2 ... 8 => read_iconst(operation_code),
-      11 ... 13 => read_fconst(operation_code),
-      18 => read_ldc(reader, indent)?,
+      9 | 10 => read_lconst_n(operation_code),
+      11 ... 13 => read_fconst_n(operation_code),
+      16 => read_bipush(reader, indent)?,
+      17 => read_sipush(reader, indent)?,
+      18 => read_ldc(reader, pool, indent)?,
+      20 => read_ldc2_w(reader, pool, indent)?,
       25 => read_aload(reader, indent)?,
       26 ... 29 => read_iload_n(operation_code),
       42 ... 45 => read_aload_n(operation_code),
       58 => read_astore(reader, indent)?,
+      75 ... 78 => read_astore_n(operation_code),
       79 => read_iastore(),
+      80 => read_lastore(),
       89 => read_dup(),
       177 => read_return(),
+      178 => read_get_static(reader, pool, indent)?,
       181 => read_put_field(reader, pool, indent)?,
       182 => read_invoke_virtual(reader, pool, indent)?,
       183 => read_invoke_special(reader, pool, indent)?,
@@ -72,16 +79,28 @@ fn read_astore(reader: &mut Reader, indent: u8) -> ReadResult {
 
   Ok(())
 }
+shortcut_op!(read_astore_n, "astore", 75, 3);
 
 single_op!(read_iastore, "iastore");
+single_op!(read_lastore, "lastore");
 
-fn read_fconst(operation: u8) {
-  let num = operation as i8 - 11;
-  match num {
-    0 ... 2 => print_op(&format!("fconst_{}", num)),
-    _ => panic!("Invalid float constant opcode {}", num)
-  }
+fn read_bipush(reader: &mut Reader, indent: u8) -> ReadResult {
+  print_op("bipush");
+  read_u8!(value, reader, indent + 1);
+  let value: i32 = value.into();
+  println!("Push int {} to stack", value);
+  Ok(())
 }
+
+fn read_sipush(reader: &mut Reader, indent: u8) -> ReadResult {
+  print_op("sipush");
+  read_u16!(value, reader, indent + 1);
+  let value: i32 = value.into();
+  println!("Push int {} to stack", value);
+  Ok(())
+}
+
+shortcut_op!(read_fconst_n, "fconst", 11, 2);
 
 fn read_iconst(operation: u8) {
   let num = operation as i8 - 3;
@@ -94,7 +113,18 @@ fn read_iconst(operation: u8) {
 
 shortcut_op!(read_iload_n, "iload", 26, 3);
 
+shortcut_op!(read_lconst_n, "lconst", 9, 1);
+
 single_op!(read_return, "return");
+
+fn read_get_static(reader: &mut Reader, pool: &PoolList, indent: u8) -> ReadResult {
+  print_op("getstatic");
+  read_u16!(field_idx, reader, indent + 1);
+  let (ref class_name, ref field_name, ref descriptor) = resolve_field_name(pool, field_idx as usize)
+    .expect(&format!("No field reference in pool at {}", field_idx));
+  println!("Get static field #{} -> {}.{}:{}", field_idx, class_name, field_name, descriptor);
+  Ok(())
+}
 
 fn read_put_field(reader: &mut Reader, pool: &PoolList, indent: u8) -> ReadResult {
   print_op("putfield");
@@ -141,10 +171,27 @@ fn read_new_array(reader: &mut Reader, indent: u8) -> ReadResult {
   Ok(())
 }
 
-fn read_ldc(reader: &mut Reader, indent: u8) -> ReadResult {
+fn read_ldc(reader: &mut Reader, pool: &PoolList, indent: u8) -> ReadResult {
   print_op("ldc");
-  read_u8!(idx, reader, indent + 1);
-  println!("load constant #{}", idx);
+  read_u8!(cst_idx, reader, indent + 1);
+  if let &Some(ref entry) = &pool[cst_idx as usize] {
+    // TODO Test that the read value is an int, short, ref, ...
+    println!("load constant #{} -> {:?}", cst_idx, entry);
+  } else {
+    panic!("No constant in the pool at {}", cst_idx);
+  }
+  Ok(())
+}
+
+fn read_ldc2_w(reader: &mut Reader, pool: &PoolList, indent: u8) -> ReadResult {
+  print_op("ldc2_w");
+  read_u16!(cst_idx, reader, indent + 1);
+  if let &Some(ref entry) = &pool[cst_idx as usize] {
+    // TODO test that the value is either an double or a long
+    println!("load constant #{} -> {:?}", cst_idx, entry);
+  } else {
+    panic!("No constant in the pool at {}", cst_idx);
+  }
   Ok(())
 }
 

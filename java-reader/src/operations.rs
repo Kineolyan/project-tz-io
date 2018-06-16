c3be1e4ef9470a50d7502e0270c8e44ab5c13f96
Kineolyan <kineolyan@gmail.com>
2018-06-16 09:13:48 +0200

    Add more operation codes.

--- java-reader/src/operations.rs
+++ java-reader/src/operations.rs
@@ -1,4 +1,4 @@
-use pool::{PoolList, resolve_method_name, resolve_class_name};
+use pool::{PoolList, resolve_method_name, resolve_class_name, resolve_field_name};
 use printer::{print_bytes};
 use reader::{Reader, ByteReader, ReadResult, to_u16};
 use types::{ARRAY_TYPES};
@@ -18,9 +18,13 @@ pub fn read(
       11 ... 13 => read_fconst(operation_code),
       18 => read_ldc(reader, indent)?,
       25 => read_aload(reader, indent)?,
+      26 ... 29 => read_iload_n(operation_code),
+      42 ... 45 => read_aload_n(operation_code),
       58 => read_astore(reader, indent)?,
       79 => read_iastore(),
-      89 => read_dup()?,
+      89 => read_dup(),
+      177 => read_return(),
+      181 => read_put_field(reader, pool, indent)?,
       182 => read_invoke_virtual(reader, pool, indent)?,
       183 => read_invoke_special(reader, pool, indent)?,
       184 => read_invoke_static(reader, pool, indent)?,
@@ -32,6 +36,26 @@ pub fn read(
   Ok(())
 }
 
+macro_rules! shortcut_op {
+  ($f: ident, $name: tt, $base: tt, $to: tt) => {
+    fn $f(operation: u8) {
+      let num = operation as i8 - $base;
+      match num {
+        0 ... $to => print_op(&format!("{}_{}", $name, num)),
+        _ => panic!("Invalid {}_n opcode {}", $name, num)
+      }
+    }
+  };
+}
+
+macro_rules! single_op {
+  ($f: ident, $name: tt) => {
+    fn $f() {
+      print_op($name);
+    }
+  }
+}
+
 fn read_aload(reader: &mut Reader, indent: u8) -> ReadResult {
   print_op("aload");
   read_u8!(var_idx, reader, indent + 1);
@@ -39,6 +63,7 @@ fn read_aload(reader: &mut Reader, indent: u8) -> ReadResult {
 
   Ok(())
 }
+shortcut_op!(read_aload_n, "aload", 42, 3);
 
 fn read_astore(reader: &mut Reader, indent: u8) -> ReadResult {
   print_op("astore");
@@ -48,9 +73,7 @@ fn read_astore(reader: &mut Reader, indent: u8) -> ReadResult {
   Ok(())
 }
 
-fn read_iastore() {
-  print_op("iastore");
-}
+single_op!(read_iastore, "iastore");
 
 fn read_fconst(operation: u8) {
   let num = operation as i8 - 11;
@@ -67,6 +90,19 @@ fn read_iconst(operation: u8) {
     0 ... 5 => print_op(&format!("iconst_{}", num)),
     _ => panic!("Invalid integer constant opcode {}", num)
   }
+}
+
+shortcut_op!(read_iload_n, "iload", 26, 3);
+
+single_op!(read_return, "return");
+
+fn read_put_field(reader: &mut Reader, pool: &PoolList, indent: u8) -> ReadResult {
+  print_op("putfield");
+  read_u16!(field_idx, reader, indent + 1);
+  let (ref class_name, ref field_name, ref descriptor) = resolve_field_name(pool, field_idx as usize)
+    .expect(&format!("No field reference in pool at {}", field_idx));
+  println!("Put field #{} = {}.{}:{}", field_idx, class_name, field_name, descriptor);
+  Ok(())
 }
 
 macro_rules! read_invoke {
@@ -112,7 +148,4 @@ fn read_ldc(reader: &mut Reader, indent: u8) -> ReadResult {
   Ok(())
 }
 
-fn read_dup() -> ReadResult {
-  print_op("dup");
-  Ok(())
-}
+single_op!(read_dup, "dup");

31b9d5f39f0fbd0a25b64a5668cf8718b2d24a98
Kineolyan <kineolyan@gmail.com>
2018-09-10 23:17:07 +0200

    Try to do some clojure code. Does it work ?

--- tzio-core/clj/src/clj/env.clj
+++ tzio-core/clj/src/clj/env.clj
@@ -1,5 +1,5 @@
 (ns clj.env
-  [:require clj.slot])
+  (:require [clj.slot as sl]))
 
 (defn create-env
   "Create a blank environment"
@@ -8,10 +8,53 @@
     [
       indexes (range 1 slot-count)
       is-input (fn [i] (some #(= i %) input-ids))
-      slots (map #(if (is-input %) (queue-slot) (empty-slot)))] 
+      slots (map #(if (is-input %) (sl/queue-slot) (sl/empty-slot)))] 
     {
       :slots slots
-      :nodes []
-      :executions []}))
+      :nodes (hash-map)
+      :executions (hash-map)}))
 
-(defn add)
+(defn add-node
+  "Adds a node to the existing system"
+  [env name memory inputs outputs operations]
+  (let 
+    [
+      nodes (:nodes env)
+      executions (:executions env)
+      new-nodes (assoc! nodes name (new-node memory inputs outputs))
+      new-executions (assoc! executions name (new-execution operations))])
+  (assoc! env 
+    :nodes new-nodes 
+    :executions new-executions))
+
+(defn new-node
+  "Creates a new node"
+  [name memory inputs outputs]
+  {
+    :memory (repeat memory 0)
+    :inputs inputs
+    :outputs outputs})
+
+(defn new-execution
+  "Create a new execution node"
+  [operations]
+  {
+    :operations operations
+    :index 0})
+
+(defn consume
+  "Feeds the environment with external data"
+  [env data]
+  (let
+    [
+      slots (:slots env)
+      idx-count (count slots)
+      indexes (filter #(sl/is-queue %) slots)
+      to-update (map vector indexes data)
+      fed-slots ()
+        (reduce
+          (fn [s (idx value)]
+            (assoc! s idx value))
+          slots
+          to-update)])  
+  (assoc! env :slots fed-slots))
--- tzio-core/clj/src/clj/slot.clj
+++ tzio-core/clj/src/clj/slot.clj
@@ -3,7 +3,7 @@
 (defn empty-slot
   "Creates an empty slot ready to receive new data"
   []
-  :empty)
+  (:empty))
 
 (defn data-slot
   "Creates a slot with an int datum inside"
@@ -13,4 +13,9 @@
 (defn queue-slot
   "Creates a slot queuing the input values"
   []
-  [])
+  (:queue []))
+
+(defmulti is-queue (fn [[slot & remaining]] slot))
+(defmethod is-queue [:queue] [_] true)
+(defmethod is-queue [:slot] [_] false)
+(defmethod is-queue [:empty] [_] false)

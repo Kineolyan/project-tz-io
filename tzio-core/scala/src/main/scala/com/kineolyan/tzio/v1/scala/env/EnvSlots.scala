9d3094ff5e1fce9ceccd8f352ff9fda7bef8c042
Kineolyan <kineolyan@gmail.com>
2018-09-19 21:48:02 +0200

    Complete consume and collect.

--- tzio-core/scala/src/main/scala/com/kineolyan/tzio/v1/scala/env/EnvSlots.scala
+++ tzio-core/scala/src/main/scala/com/kineolyan/tzio/v1/scala/env/EnvSlots.scala
@@ -1,6 +1,11 @@
 package com.kineolyan.tzio.v1.scala.env
 
-class EnvSlots(val slots: Array[Any], val inputs: Array[Int], val outputs: Array[Int]) {}
+class EnvSlots(val slots: Array[Any], val inputs: Array[Int], val outputs: Array[Int]) {
+
+  def copy(slots: Array[Any] = slots, inputs: Array[Int] = inputs, outputs: Array[Int] = outputs) =
+    new EnvSlots(slots, inputs, outputs)
+
+}
 
 object EnvSlots {
   def empty(): EnvSlots = new EnvSlots(Array(), Array(), Array())

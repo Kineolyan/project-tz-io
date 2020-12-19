0ecb26dc5e9154787a5837dc0298b8f72f6c2566
Kineolyan <kineolyan@protonmail.com>
2020-12-19 20:23:57 +0100

    language is now clean.

--- rs-parser/language/src/address.rs
+++ rs-parser/language/src/address.rs
@@ -2,63 +2,59 @@ use std::fmt;
 
 #[derive(PartialEq)]
 pub enum Node {
-	In,
-	Out,
-	Node(String),
+    In,
+    Out,
+    Node(String),
 }
 
 impl fmt::Debug for Node {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		self.do_fmt(f)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.do_fmt(f)
+    }
 }
 
 impl fmt::Display for Node {
-	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		self.do_fmt(f)
-	}
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.do_fmt(f)
+    }
 }
 
 impl Node {
+    pub fn new_node(name: &str) -> Self {
+        Node::Node(name.to_string())
+    }
 
-	pub fn new_node(name: &str) -> Self {
-		Node::Node(name.to_string())
-	}
+    pub fn get_id(&self) -> &String {
+        match self {
+            Node::Node(ref id) => id,
+            _ => panic!("Not a named node: {}", self),
+        }
+    }
 
-	pub fn get_id<'a>(&'a self) -> &'a String {
-		match self {
-			&Node::Node(ref id) => id,
-			_ => panic!("Not a named node: {}", self),
-		}
-	}
-
-	fn do_fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-		match self {
-			&Node::In => write!(f, "<IN>"),
-			&Node::Out => write!(f, "<OUT>"),
-			&Node::Node(ref id) => write!(f, "Node#{}", id),
-		}
-	}
+    fn do_fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            Node::In => write!(f, "<IN>"),
+            Node::Out => write!(f, "<OUT>"),
+            Node::Node(ref id) => write!(f, "Node#{}", id),
+        }
+    }
 }
 
 #[derive(Debug, PartialEq)]
 pub struct Port {
-	pub node: Node,
-	pub port: u32,
+    pub node: Node,
+    pub port: u32,
 }
 
 impl Port {
-	pub fn new(node: Node, port: u32) -> Self {
-		Port {
-			node: node,
-			port: port,
-		}
-	}
+    pub fn new(node: Node, port: u32) -> Self {
+        Port { port, node }
+    }
 
-	pub fn named_port(node_name: &str, port: u32) -> Self {
-		Port {
-			node: Node::new_node(node_name),
-			port: port,
-		}
-	}
+    pub fn named_port(node_name: &str, port: u32) -> Self {
+        Port {
+            node: Node::new_node(node_name),
+            port,
+        }
+    }
 }

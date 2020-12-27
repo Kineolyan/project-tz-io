3124966aa0db55f3bb69e5a8c56d07e182d057fc
Olivier Peyrusse <ope@activeviam.com>
2020-12-27 15:30:45 +0100

    Drop scala core, not wanted anymore.
    It was fun but I don't want to maintain it anymore. Not to lose time
    maintaining it, I will drop it.

--- tzio-core/scala/src/main/scala/com/kineolyan/tzio/v1/scala/slot/InputSlot.scala
+++
@@ -1,9 +0,0 @@
-package com.kineolyan.tzio.v1.scala.slot
-
-/**
-  * Trait defining an environment input slot
-  */
-trait InputSlot {
-  def canRead: Boolean
-  def read(): (Int, InputSlot)
-}
--- tzio-core/scala/src/main/scala/com/kineolyan/tzio/v1/scala/slot/OutputSlot.scala
+++
@@ -1,9 +0,0 @@
-package com.kineolyan.tzio.v1.scala.slot
-
-/**
-  * Trait defining an environment output slot
-  */
-trait OutputSlot {
-  def canWrite: Boolean
-  def write(value: Int): OutputSlot
-}
